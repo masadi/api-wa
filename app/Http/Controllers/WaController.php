f67601311eb7cb38d23c54e5c7f2b360e4e1771b
masadi <masadi.com@gmail.com>
2023-02-24 20:06:47 +0700

    Update WaController.php

--- app/Http/Controllers/WaController.php
+++ app/Http/Controllers/WaController.php
@@ -12,23 +12,23 @@ class WaController extends Controller
     public function index(){
         $rawdata = file_get_contents("php://input");
 		$json = json_decode($rawdata, true);
-        Storage::disk('public')->put('test.json', json_encode($json));
+        /*Storage::disk('public')->put('test.json', json_encode($json));
         Storage::disk('public')->put('sender.txt', $json['sender']);
         Storage::disk('public')->put('sender_phone.txt', $json['sender_phone']);
         Storage::disk('public')->put('chat.txt', $json['chat']);
         Storage::disk('public')->put('sender_push_name.txt', $json['sender_push_name']);
         Storage::disk('public')->put('message_id.txt', $json['message_id']);
         Storage::disk('public')->put('sender.txt', $json['sender']);
-        Storage::disk('public')->put('sender.txt', $json['sender']);
-        if(!$json['is_from_me']){
-
+        Storage::disk('public')->put('sender.txt', $json['sender']);*/
+        $blok = ['6285231444789', '6285231548456'];
+        if(in_array($json['sender_phone'], $blok)){
+            Artisan::call('kirim:wa', [
+                'no' => ($json['is_group']) ? $json['chat'] : $json['sender_phone'],
+                'text' => 'ini reply text',
+                'is_group' => $json['is_group'],
+                'nama' => $json['sender_push_name'],
+            ]);
         }
-        Artisan::call('kirim:wa', [
-            'no' => ($json['is_group']) ? $json['chat'] : $json['sender_phone'],
-            'text' => 'ini reply text',
-            'is_group' => $json['is_group'],
-            'nama' => $json['sender_push_name'],
-        ]);
         $data = [];
         return response()->json($data);
     }
