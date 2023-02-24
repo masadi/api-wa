e1a04af1d6750e103847aee3b3e4b2df6d87e4ab
masadi <masadi.com@gmail.com>
2023-02-24 19:25:32 +0700

    Update WaController.php

--- app/Http/Controllers/WaController.php
+++ app/Http/Controllers/WaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Storage;
+use Artisan;
 
 class WaController extends Controller
 {
@@ -12,7 +13,19 @@ class WaController extends Controller
         $rawdata = file_get_contents("php://input");
 		$json = json_decode($rawdata, true);
         Storage::disk('public')->put('test.json', json_encode($json));
-        Storage::disk('public')->put('nama.txt', $json['chat']);
+        Storage::disk('public')->put('sender.txt', $json['sender']);
+        Storage::disk('public')->put('sender_phone.txt', $json['sender_phone']);
+        Storage::disk('public')->put('sender_push_name.txt', $json['sender_push_name']);
+        Storage::disk('public')->put('message_id.txt', $json['message_id']);
+        Storage::disk('public')->put('sender.txt', $json['sender']);
+        Storage::disk('public')->put('sender.txt', $json['sender']);
+        if(!$json['is_from_me']){
+
+        }
+        Artisan::call('kirim:wa', [
+            'no' => $json['sender_phone'],
+            'text' => 'ini reply text',
+        ]);
         $data = [];
         return response()->json($data);
     }
