653d6968ff73b6ae2b7220b887b480eae7ea2cd8
masadi <masadi.com@gmail.com>
2023-02-24 18:45:05 +0700

    Update WaController.php

--- app/Http/Controllers/WaController.php
+++ app/Http/Controllers/WaController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Storage;
 
 class WaController extends Controller
 {
     public function index(){
+        Storage::disk('public')->put('test.json', json_encode(request()->all()));
         $data = [];
         return response()->json($data);
     }
