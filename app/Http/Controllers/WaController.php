587df865857839bd7dd229210e124b14b343d453
masadi <masadi.com@gmail.com>
2023-02-24 19:15:14 +0700

    Update WaController.php

--- app/Http/Controllers/WaController.php
+++ app/Http/Controllers/WaController.php
@@ -9,9 +9,9 @@ use Storage;
 class WaController extends Controller
 {
     public function index(){
-        $rawdata = file_get_contents("php://input");
-		$json = json_decode($rawdata, true);
-        Storage::disk('public')->put('test.json', $json);
+        //$rawdata = file_get_contents("php://input");
+		//$json = json_decode($rawdata, true);
+        Storage::disk('public')->put('test.json', json_decode(request()->all()));
         $data = [];
         return response()->json($data);
     }
