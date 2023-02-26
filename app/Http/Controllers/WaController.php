1e97b9c1c18e30bfe45855b4f9f2586235fc3e25
masadi <masadi.com@gmail.com>
2023-02-26 08:11:54 +0700

    Update WaController.php

--- app/Http/Controllers/WaController.php
+++ app/Http/Controllers/WaController.php
@@ -9,6 +9,11 @@ use Artisan;
 
 class WaController extends Controller
 {
+    public function __construct()
+    {
+        $this->api_url = config('wa.api_url');
+        $this->api_key = config('wa.api_key');
+    }
     public function index(){
         $rawdata = file_get_contents("php://input");
 		$json = json_decode($rawdata, true);
@@ -21,13 +26,22 @@ class WaController extends Controller
         Storage::disk('public')->put('sender.txt', $json['sender']);
         Storage::disk('public')->put('sender.txt', $json['sender']);*/
         $blok = ['6285231444789', '6285231548456'];
-        if(in_array($json['sender_phone'], $blok)){
-            Artisan::call('kirim:wa', [
+        if(!in_array($json['sender_phone'], $blok)){
+            /*Artisan::call('kirim:wa', [
                 'no' => ($json['is_group']) ? $json['chat'] : $json['sender_phone'],
                 'text' => 'ini reply text',
                 'is_group' => $json['is_group'],
                 'nama' => $json['sender_push_name'],
-            ]);
+            ]);*/
+            $data_post = [
+                "recipient_type" => ($json['is_group']) ? "group" : "individual",
+                "to" => ($json['is_group']) ? $json['chat'] : $json['sender_phone'],
+                "type" => "text",
+                "text" => [
+                    "body" => 'Halo '.$json['sender_push_name'].'. Ini adalah jawaban otomatis'
+                ]
+            ];
+            $response = Http::withToken($token)->post($url, $data_post);
         }
         $data = [];
         return response()->json($data);
